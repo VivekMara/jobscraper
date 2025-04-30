0ba5ed12cbb327d830fd56ede067c578f09f781f
VivekMara <vivek.v.mara@gmail.com>
2025-04-30 19:35:29 +0530

    minor fixes

---
+++ cmd/helpers/notifier.go
@@ -0,0 +1,7 @@
+package helpers
+
+import "fmt"
+
+func Notifier() {
+	fmt.Println("Hello from notifier")
+}
--- cmd/main.go
+++ cmd/main.go
@@ -1,24 +1,10 @@
 package main
 
 import (
-	"fmt"
-
-	"github.com/gocolly/colly"
+	"jobscraper/cmd/helpers"
 )
 
-func main(){
-	c := colly.NewCollector(
-	)
-
-	c.OnHTML("a[href]", func(h *colly.HTMLElement) {
-		link := h.Attr("href")
-		fmt.Printf("Link found: %q -> %s", h.Text, link)
-		c.Visit(h.Request.AbsoluteURL(link))
-	})
-	
-	c.OnRequest(func(r *colly.Request) {
-		fmt.Println("Visiting", r.URL.String())
-	})
-
-	c.Visit("https://wikipedia.com")
+func main() {
+	helpers.Notifier()
+	helpers.Scraper()
 }
