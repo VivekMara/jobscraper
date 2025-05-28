77c40a35ab2d7e665abbc1329112c19f2e658664
VivekMara <vivek.v.mara@gmail.com>
2025-05-28 16:24:39 +0000

    init

--- cmd/helpers/notifier.go
+++
@@ -1,7 +0,0 @@
-package helpers
-
-import "fmt"
-
-func Notifier() {
-	fmt.Println("Hello from notifier")
-}
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from jobscraper!")
+
+
+if __name__ == "__main__":
+    main()
